c09757cfe003362a0de105a016b3a1bdc65374aa
mileslilly2 <mileslilly2@gmail.com>
2024-02-14 17:32:27 -0600

    tacos

--- pages/2_Lesson_2_Numerical_processing_with_NumPy.py
+++ pages/2_Lesson_2_Numerical_processing_with_NumPy.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
-from utilities import load_css, read_md
+#from utilities import load_css, read_md
 
-st.set_page_config(page_title="Lesson 2 - Numerical processing with NumPy", page_icon="📖", layout="wide")
+#st.set_page_config(page_title="Lesson 2 - Numerical processing with NumPy", page_icon="📖", layout="wide")
 
-load_css()
+#load_css()
 
-md = read_md('Lesson-2.md')
-st.markdown(md, unsafe_allow_html=True)
+#md = read_md('Lesson-2.md')
+#st.markdown(md, unsafe_allow_html=True)
