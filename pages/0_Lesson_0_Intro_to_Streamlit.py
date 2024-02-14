4e54b2edfdd14808d920dfb9a5c0e7c0eb2f1b77
mileslilly2 <mileslilly2@gmail.com>
2024-02-14 16:17:46 -0600

    first commit

--- pages/0_Lesson_0_Intro_to_Streamlit.py
+++ pages/0_Lesson_0_Intro_to_Streamlit.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import pandas as pd
-from utilities import load_css, read_md
+#from utilities import load_css, read_md
 
 st.set_page_config(page_title="Lesson 0 - Getting up to speed with Streamlit", page_icon="📖", layout="wide")
 
-load_css()
+#load_css()
 
-md = read_md('Lesson-0.md')
-st.markdown(md, unsafe_allow_html=True)
+#md = read_md('Lesson-0.md')
+#st.markdown(md, unsafe_allow_html=True)
 
 
 st.divider()
